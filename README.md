39208c7195ed3ad54f7c53b5bbe87bfa0a5d4f22
Mark Lee <leemark@gmail.com>
2024-03-08 23:28:32 -0700

    first

---
+++ README.md
@@ -0,0 +1,9 @@
+# Answer Engine
+search + rag + reflection + summarization + followup qs
+
+uses openai embeddings, brave for search, and groq + mixtral for inference
+
+## How to install 
+pip install -r requirements.txt
+
+
---
+++ app.py
@@ -0,0 +1,31 @@
+
+# Import the dotenv module to load environment variables from a .env file
+from dotenv import load_dotenv
+# Load environment variables
+load_dotenv()
+
+# https://python.langchain.com/docs/integrations/text_embedding/openai
+# import OpenAIEmbeddings to embed text
+from langchain_openai import OpenAIEmbeddings
+# create an instance of OpenAIEmbeddings  
+embed_model = OpenAIEmbeddings(model="text-embedding-3-small")
+
+# https://python.langchain.com/docs/integrations/chat/groq
+# import ChatGroq and ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_groq import ChatGroq
+
+# create an instance of ChatGroq
+chat = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768")
+# create an instance of ChatPromptTemplate
+system = """You are a rephraser and always respond ONLY with a rephrased version of the userinput 
+            that will given to a search engine API. Always be succint and use the same words 
+            as the input.  ONLY RETURN THE REPHRASED VERSION OF THE USER INPUT WITH NO OTHER TEXT.
+            """
+human = "{text}"
+prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
+
+chain = prompt | chat
+response = chain.invoke({"text": "Write a sonnet about the lord of the rings."})
+
+print(response)
